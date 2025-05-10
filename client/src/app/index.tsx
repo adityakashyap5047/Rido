2e64904892982112eb1f3380ab1078f0e2d500ce
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-11 01:11:49 +0530

    successfully run the app

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Main = () => {
   return (
     <View>
-      <Text>Main</Text>
+      <Text>Congratulations!!! You Successfully Run the app</Text>
     </View>
   )
 }
