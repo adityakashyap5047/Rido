0646c167b2823c55e0ffd1a03505d9c27fe31fca
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-12 17:00:38 +0530

    starting with the index page

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font'
+import { commonStyles } from '@/styles/commonStyles'
+import { splashStyles } from '@/styles/splashStyles'
 
 const Main = () => {
+
+  const [loaded] = useFonts({
+    Bold: require("@/assets/fonts/NotoSans-Bold.ttf"),
+    Regular: require("@/assets/fonts/NotoSans-Regular.ttf"),
+    Medium: require("@/assets/fonts/NotoSans-Medium.ttf"),
+    Light: require("@/assets/fonts/NotoSans-Light.ttf"),
+    SemiBold: require("@/assets/fonts/NotoSans-SemiBold.ttf"),
+  })
+
   return (
-    <View>
+    <View style={commonStyles.container}>
+      <Image 
+        source={require("@/assets/images/logo_t.png")}
+        style={splashStyles.img}
+      />
       <Text>Congratulations!!! You Successfully Run the app</Text>
     </View>
   )
