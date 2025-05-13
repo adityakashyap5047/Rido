cc7e7727402532fcb6c54fc34fba35f07a193ad3
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-13 10:10:57 +0530

    created the storage using mmkvStorage

---
+++ client/src/store/storage.tsx
@@ -0,0 +1,28 @@
+import {MMKV} from 'react-native-mmkv'
+
+export const tokenStorage = new MMKV({
+    id: 'tokenStorage',
+    encryptionKey: 'SOME-RANDOM-SUPER-SECRECT-KEY',
+})
+
+export const storage = new MMKV({
+    id: 'my-app-storage',
+    encryptionKey: 'SOME-RANDOM-SUPER-SECRECT-KEY',
+})
+
+export const mmkvStorage: {
+    setItem: (key: string, value: string) => void,
+    getItem: (key: string) => string | null,
+    removeItem: (key: string) => void,
+} = {
+    setItem: (key: string, value: string) => {
+        storage.set(key, value)
+    },
+    getItem: (key: string) => {
+        const value = storage.getString(key)
+        return value ?? null;
+    },
+    removeItem: (key: string) => {
+        storage.delete(key)
+    },
+}
