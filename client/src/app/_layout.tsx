3ad5c56bea26dfca60f0488cc41802e384f755a2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-13 11:29:13 +0530

    wrapping up the app with WSProvider

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Stack } from 'expo-router'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+import { WSProvider } from '@/service/WSProvider'
 
 const Layout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index"/>
-      <Stack.Screen name="role"/>
-    </Stack>
+    <WSProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index"/>
+        <Stack.Screen name="role"/>
+      </Stack>
+    </WSProvider>
   )
 }
 
-export default Layout
+export default gestureHandlerRootHOC(Layout)
