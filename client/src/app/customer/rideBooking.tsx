f6fbeb6fcfe6619ded0ce2ec72200bb49ddcd014
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-15 23:21:26 +0530

    starting with ride booking

--- client/src/app/customer/rideBooking.tsx
+++ client/src/app/customer/rideBooking.tsx
@@ -1,12 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { memo, useState } from 'react'
+import { useRoute } from '@react-navigation/native'
+import { useUserStore } from '@/store/userStore'
 
-const rideBooking = () => {
+const RideBooking = () => {
+
+    const route = useRoute() as any
+    const item = route?.param as any
+    const {location} = useUserStore() as any
+    const [selectedOption, setSelectedOption] = useState("Bike")
+    const [loading, setLoading] = useState(false)
+
   return (
     <View>
-      <Text>rideBooking</Text>
+      <Text>RideBooking</Text>
     </View>
   )
 }
 
-export default rideBooking
+export default memo(RideBooking)
