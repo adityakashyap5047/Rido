dde6a124257dbc6de6d5172e8da1f59844b2f184
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-15 11:21:51 +0530

    adding the back button in location selection

--- client/src/app/customer/SheetContent.tsx
+++ client/src/app/customer/SheetContent.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { uiStyles } from '@/styles/uiStyles'
--- client/src/app/customer/selectlocations.tsx
+++ client/src/app/customer/selectlocations.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useUserStore } from '@/store/userStore'
+import { homeStyles } from '@/styles/homeStyles'
+import { StatusBar } from 'expo-status-bar'
+import { commonStyles } from '@/styles/commonStyles'
+import { router } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '@/utils/Constants'
+import CustomText from '@/components/shared/CustomText'
 
 const LocationSelection = () => {
+
+  const{location, setLocation} = useUserStore()
+
+  const [pickup, setPickup] = useState("")
+  const [pickupCoords, setPickupCoords] = useState<any>(null)
+  const [dropCoords, setDropCoords] = useState<any>(null)
+  const [drop, setDrop] = useState("")
+  const [locations, setLocations] = useState([])
+  const [focusedInput, setFocusedInput] = useState("drop")
+  const [modalTitle, setModalTitle] = useState("drop")
+  const [isMapModalVisible, setIsMapModalVisible] = useState(false)
+
   return (
-    <View>
-      <Text>LocationSelection</Text>
+    <View style={homeStyles.container}>
+      <StatusBar style='light' backgroundColor='orange' translucent={false} />
+      <SafeAreaView />
+
+      <TouchableOpacity
+        style={commonStyles.flexRow}
+        onPress={() => router.back()}
+      >
+        <Ionicons name='chevron-back' size={18} color={Colors.iosColor}/>
+        <CustomText fontFamily='Regular' style={{color: Colors.iosColor}}>
+          Back
+        </CustomText>
+      </TouchableOpacity>
     </View>
   )
 }
